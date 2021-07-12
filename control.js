299ed95b7ecaf1e43f6e9474205417d0740b7492
Xebba <xebba163@gmail.com>
2021-07-12 07:30:22 -0700

    done the changes

--- control.js
+++ control.js
@@ -1,17 +1,17 @@
-let count = 0;
-
+let digit = 0;
 const buttonUp = document.getElementById("countUp");
 const buttonDown = document.getElementById("countDown");
 const num = document.getElementById("number");
-num.innerHTML = count;
+num.innerHTML = digit;
 
 buttonUp.addEventListener("click", function() {
- num.innerHTML = count++;
+ num.innerHTML = digit++;
 });
   
 buttonDown.addEventListener("click", function() {
-num.innerHTML = --count;   
+num.innerHTML = --digit;   
 });
+
 
 
 
